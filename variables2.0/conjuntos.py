55805e237d030054bf8ce196d73d4f3c99b51cfb
Cristian Romero <cristian.romerogarcia10@gmail.com>
2024-08-06 17:29:26 -0600

    agregando el complemento de conjuntos a la carpeta variables2

--- variables2.0/conjuntos.py
+++ variables2.0/conjuntos.py
@@ -1,5 +1,26 @@
 
 #creando un conjunto con set()
-conjunto = set(["Dato 1","Dato 2"])
+conjunto = set(["Dato 1",])
 
-print(conjunto)
+#metiendo un conjunto dentro de otro conjunto
+conjunto1 = frozenset(["dato 1","dato 2"])
+conjunto2 = {conjunto1, "dato 3"}
+#print(conjunto2)
+
+#Teoria de conjuntos 
+conjunto1 = {1,3,5,7}
+conjunto2 = {1,3,7}
+
+#Verificando si es un subconjunto
+resultado = conjunto2.issubset(conjunto1)
+resultado = conjunto2 <= conjunto1
+
+#Verificando si es un superconjunto
+resultado = conjunto2.issuperset(conjunto1)
+resultado = conjunto1 > conjunto2
+
+#verificar si hay algun numeró en comun
+resultado = conjunto2.isdisjoint(conjunto1)
+
+print(resultado)
+
