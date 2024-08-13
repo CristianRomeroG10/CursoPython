dabe4e8bba38f7910968c574717869caa4bf333c
Cristian Romero <cristian.romerogarcia10@gmail.com>
2024-08-12 18:20:34 -0600

    resolviendo problema con archivos csv

---
+++ archivos_problemas_resueltos/problemas_csv.py
@@ -0,0 +1,24 @@
+
+#Cambiar el tipo de dato de una columna
+import pandas as pd
+
+df = pd.read_csv("archivos_problemas_resueltos/datos.csv")
+
+#convertir a string los datos de una columna
+df['edad'] = df['edad'].astype(str)
+#mostar el tipo de dato del primer elemento de la columna edad
+#print(type(df['edad'][0]))
+
+#Reemplazando los dato "dalto" por "Maestro"
+df['apellido'].replace("Romero","maestro",inplace=True)
+
+#eliminando las filas con datos faltantes
+df = df.dropna()
+
+#eliminando las filas repetidas
+df = df.drop_duplicates()
+
+#creando un CSv con el dataframe resultante (limpio)
+df.to_csv("archivos_problemas_resueltos/datos_limpios.csv")
+
+
